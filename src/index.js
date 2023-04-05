9d7e40faaac3d3a20513a80456c2a6e3948be47d
Agnes Udoh <udohlucee@yahoo.com>
2023-04-05 22:08:16 +0100

    Google page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import GooglePage from './components/Googlepage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GooglePage />
   </React.StrictMode>
 );
 
