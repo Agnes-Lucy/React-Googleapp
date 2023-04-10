10bccabb911e9542a58b542584a72fce8b4165fb
Agnes Udoh <udohlucee@yahoo.com>
2023-04-10 16:29:00 +0100

    useState Hook

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,8 +1,25 @@
+import { useState } from "react"
 import "../styles/center.css"
+
 const Searchbar = () => {
+    const [text, setText] = useState("");
+
+    function handleTextChange(e) {
+        setText(e.target.value);
+    } 
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            e.preventDefault();
+        }
+    }
+
     return (
         <div>
-            <input type={"text"} id="input"/>
+            <form onKeyDown={handleKeyDown}>
+                 <input type="text" id="input" value={text} onChange={handleTextChange}/>
+            </form>
+            
         </div>
     )
 }
