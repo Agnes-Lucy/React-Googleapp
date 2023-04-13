c3a79608a713114dad42c6b251ad8335c1155646
Agnes Udoh <udohlucee@yahoo.com>
2023-04-13 19:56:04 +0100

    correction

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -10,7 +10,7 @@ const Searchbar = () => {
 
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
-            e.preventDefault();
+            event.preventDefault();
         }
     }
 
